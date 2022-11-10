ee36e99cd1b4f2698c29d19c633e59469704770f
Shivakant6387 <shivakant5875@gmail.com>
2022-11-10 14:05:55 +0530

    My First Spring project

---
+++ src/test/java/com/example/ToDoListFirst/ToDoListFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ToDoListFirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
